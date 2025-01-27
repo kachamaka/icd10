53d9dd2c82df76376cec58bd209e31b24f03a8f5
unknown <martin.popov42@gmail.com>
2025-01-27 23:13:26 +0200

    Add log

--- handlers/searchBySymptoms.go
+++ handlers/searchBySymptoms.go
@@ -22,6 +22,7 @@ func SearchBySymptomsHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, fmt.Sprintf("Error parsing JSON: %v", err), http.StatusBadRequest)
 		return
 	}
+	log.Println("Query:", req.Query)
 
 	icd10Codes, err := elastic.SearchBySymptoms(req.Query)
 	if err != nil {
